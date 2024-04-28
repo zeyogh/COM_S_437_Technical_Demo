07eaa786200ad83e42f20ebf0146b22f32f922e8
zeyogh <zeynep.ogh@gmail.com>
2024-04-28 18:17:10 -0500

    Checks for "pirated" copies

--- COMS437TechnicalDemo/Assets/Scripts/IsItPirated.cs
+++ COMS437TechnicalDemo/Assets/Scripts/IsItPirated.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class IsItPirated : MonoBehaviour
 {
     [SerializeField] PlayerMovement playerMovement;
+    [SerializeField] TextMeshProUGUI text;
 
     private string[] allowedHosts =
     {
@@ -17,19 +19,22 @@ public class IsItPirated : MonoBehaviour
 
     private void Start()
     {
+        if (Application.absoluteURL.Length > 0)
+            text.SetText(Application.absoluteURL);
+        else
+            text.SetText("Nothing to see here...");
         if (!Application.isEditor)
             ValidateURL();
     }
 
     private void ValidateURL()
     {
-        if (!IsValidURL(allowedHosts))
+        if (!IsValidURL(allowedHosts) && Application.absoluteURL.Length > 0)
         {
             playerMovement.setJumpable(false);
             GameObject.FindWithTag("TrueTerrain").SetActive(false);
             GameObject.FindWithTag("BrokenTerrain1").SetActive(true);
             GameObject.FindWithTag("BrokenTerrain2").SetActive(true);
-            //SceneManager.LoadScene("PirateScene");
         }
     }
 
